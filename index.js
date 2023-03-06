b68ef3d8fd9ec3b046e074eef22a20893c5bf2a7
RAHU <tiwarirahul0809@gmail.com>
2023-03-06 16:38:07 +0530

    performance update

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const inputBtn = document.getElementById("input-btn")
+let inputEl = document.getElementById("input-el")
+let myLead = ["www.awsomelead.com","www.lead2.com", "www.greatelead.com"]
+let ulEl = document.getElementById("ul-el")
 
-function saveLead(){
-    console.log('Button clicked');
+inputBtn.addEventListener("click", function() {
+    myLead.push(inputEl.value)
+})
+
+let list = ""
+for (let i = 0; i < myLeads.length; i++) {
+    list += "<li>" + myLeads[i] + "</li>"
 }
+ulEl.innerHTML = list
