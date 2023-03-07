05237e4dd699ddaabc98915f0caa81aa17fa4fe2
RAHU <tiwarirahul0809@gmail.com>
2023-03-07 15:44:54 +0530

    lead tracker fully operational

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
+let myLead = []
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
-let myLead = []
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 
-inputBtn.addEventListener("click", function() {
-    myLead.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
-})
-function renderLeads(){
+if (leadsFromLocalStorage) {
+    myLead = leadsFromLocalStorage
+    render(myLead)
+}
+function render(lead){
     let list = ""
-    for (let i = 0; i < myLead.length; i++) {
+    for (let i = 0; i < lead.length; i++) {
         list += `
             <li>
-                <a target="_blank" href="${myLead[i]}">
-                    ${myLead[i]}
+                <a target="_blank" href="${lead[i]}">
+                    ${lead[i]}
                 </a>
             </li>
         `
     }
     ulEl.innerHTML = list
 }
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear()
+    myLead = []
+    ulEl.innerHTML = ""
+})
+inputBtn.addEventListener("click", function() {
+    myLead.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLead))
+    render(myLead)
+})
+
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+        console.log(tabs[0].url);
+        myLead.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLead))
+        render(myLead)
+    });
+})
